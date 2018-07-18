4d05932a8f7f175d77e4859236ed6ab11aa33399
Armen <rissmiller.a@husky.neu.edu>
2018-07-18 10:26:56 -0400

    added more options when asked for pin

--- app/voya401k.js
+++ app/voya401k.js
@@ -5,6 +5,15 @@ var XLSX = require('xlsx');
 const inputId = '2222';
 const VERSION = '1.0';
 
+
+/**
+Question Numbers:
+    0 - PIN
+    1 - Would you like to hear suggestions to help you retire sooner?
+    2 - reccomends that you increase savings by 2%, yes/no
+    3 - option to save 1% more a year from now
+**/
+
 module.exports = function(req, res) {
 
     console.log('New request for the Voya 401k:\n', req.body);
@@ -63,7 +72,96 @@ module.exports = function(req, res) {
             false )
           );
       }
-		}
+		}else if (req.body.request.intent.name === 'VoyaNoIntent') {
+      var dataRow = readData(req.body.session.attributes.voyaPin);
+      var question = req.body.session.attributes.questionNo;
+      if(question === '1') {
+        res.json(
+            buildResponse(
+              {},
+              '<speak>Ok '+dataRow.FirstName+'!, I understand thank you for using Voya 401k service, have a nice day!</speak>',
+              {},
+              '',
+              true )
+            );
+      }
+      else if(question === '2') {
+        res.json(
+            buildResponse(
+              {questionNo: '3', voyaPin: dataRow.No},
+              '<speak>Ok, I understand. Would you want to save more in the future? '
+              +'I can sign you up to save 1% more a year from now?</speak>',
+              {},
+              '',
+              false )
+            );
+      }
+      else {
+        res.json(
+            buildResponse(
+              {},
+              '<speak>Ok '+dataRow.FirstName+'!, I understand thank you for using Voya 401k service, have a nice day!</speak>',
+              {},
+              '',
+              true )
+            );
+      }
+    } else if (req.body.request.intent.name === 'VoyaYesIntent') {
+      var question = req.body.session.attributes.questionNo;
+      if(question == '0') {
+        res.json(
+          buildResponse(
+            //maybe we should calculate these values instead of pulling them
+            //from the spreadsheet
+            {questionNo: '0'},
+            '<speak>OK, just say the PIN</speak>',
+            {},
+            '',
+            false)
+        );
+      }
+      var dataRow = readData(req.body.session.attributes.voyaPin);
+      if(question === '1') {
+        res.json(
+          buildResponse(
+            //maybe we should calculate these values instead of pulling them
+            //from the spreadsheet
+            {questionNo: '2', voyaPin : dataRow.No},
+            '<speak>You are doing a great job of saving '
+             + dataRow.CurrentSaving + ' from your pay.' +
+             ' if you increase your savings rate to' + dataRow.IncreaseSaving +
+             ' you could retire at age ' + dataRow.ActualAge + '. Would you like to'
+             +' increase your savings rate by '+ dataRow.SavingsRate + ' now?'
+             + '</speak>',
+            {},
+            '',
+            false)
+        );
+      }
+      else if(question === '2' || question === '3') {
+        res.json(
+          buildResponse(
+            //maybe we should calculate these values instead of pulling them
+            //from the spreadsheet
+            {voyaPin : dataRow.No},
+            '<speak>OK, great. I\'ve done that for you. Congratulations,' +
+            ' your future self will thank you!</speak>',
+            {},
+            '',
+            true)
+        );
+      }
+      else {
+        res.json(
+        buildResponse(
+          {questionNo: question, voyaPin : dataRow.No},
+          '<speak>I\'m sorry?</speak>',
+          {},
+          '',
+          false)
+      );
+      }
+    }
     else if (!req.body.session.attributes.hasOwnProperty('voyaPin')) {
       res.json(
         buildResponse(
@@ -87,84 +185,7 @@ module.exports = function(req, res) {
 						false )
 					);
 
-			} else if (req.body.request.intent.name === 'VoyaNoIntent') {
-        var dataRow = readData(req.body.session.attributes.voyaPin);
-        var question = req.body.session.attributes.questionNo;
-        if(question === '1') {
-          res.json(
-              buildResponse(
-                {},
-                '<speak>Ok '+dataRow.FirstName+'!, I understand thank you for using Voya 401k service, have a nice day!</speak>',
-                {},
-                '',
-                true )
-              );
-        }
-        else if(question === '2') {
-          res.json(
-              buildResponse(
-                {questionNo: '3', voyaPin: dataRow.No},
-                '<speak>Ok, I understand. Would you want to save more in the future? '
-                +'I can sign you up to save 1% more a year from now?</speak>',
-                {},
-                '',
-                false )
-              );
-        }
-        else {
-          res.json(
-              buildResponse(
-                {},
-                '<speak>Ok '+dataRow.FirstName+'!, I understand thank you for using Voya 401k service, have a nice day!</speak>',
-                {},
-                '',
-                true )
-              );
-        }
-			} else if (req.body.request.intent.name === 'VoyaYesIntent') {
-        var dataRow = readData(req.body.session.attributes.voyaPin);
-        var question = req.body.session.attributes.questionNo;
-        if(question === '1') {
-          res.json(
-            buildResponse(
-              //maybe we should calculate these values instead of pulling them
-              //from the spreadsheet
-              {questionNo: '2', voyaPin : dataRow.No},
-              '<speak>You are doing a great job of saving '
-               + dataRow.CurrentSaving + ' from your pay.' +
-               ' if you increase your savings rate to' + dataRow.IncreaseSaving +
-               ' you could retire at age ' + dataRow.ActualAge + '. Would you like to'
-               +' increase your savings rate by '+ dataRow.SavingsRate + ' now?'
-               + '</speak>',
-              {},
-              '',
-              false)
-          );
-        }
-        else if(question === '2' || question === '3') {
-          res.json(
-            buildResponse(
-              //maybe we should calculate these values instead of pulling them
-              //from the spreadsheet
-              {voyaPin : dataRow.No},
-              '<speak>OK, great. I\'ve done that for you. Congratulations,' +
-              ' your future self will thank you!</speak>',
-              {},
-              '',
-              true)
-          );
-        }
-        else {
-          res.json(
-          buildResponse(
-            {questionNo: question, voyaPin : dataRow.No},
-            '<speak>I\'m sorry?</speak>',
-            {},
-            '',
-            false)
-        );
-        }
-      } else if (req.body.request.type === 'HelpIntent') {
+			}  else if (req.body.request.type === 'HelpIntent') {
 				res.json(
 					buildResponse(
 						{},
